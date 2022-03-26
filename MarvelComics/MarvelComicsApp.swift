a15bfdd059cb947b0037a52eac0cf54ca52ca6d1
konradcr <63312007+konradcr@users.noreply.github.com>
2022-03-26 15:36:10 +0100

    Initial Commit

---
+++ MarvelComics/MarvelComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsApp.swift
+//  MarvelComics
+//
+//  Created by Konrad Cureau on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
